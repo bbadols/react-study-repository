6a51650569f41dd6bbd572d74c4bc5b57c98e62a
kgy-inst <bestyoona1@gmail.com>
2025-09-10 15:45:56 +0900

    feat:lesson2,3 done

--- lesson2-basic-react-app-component/src/main.jsx
+++ lesson2-basic-react-app-component/src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// index.html의 root div를 찾아 렌더링(화면이 보여지게 된다)
 createRoot(document.getElementById('root')).render(
+  // StrictMode : 개발모드에만 작동, 오류에 대한 사전 테스트
   <StrictMode>
     <App />
   </StrictMode>,
