b93e6d4e3116780054de315e5dbcd4effc91b59e
kgy-inst <bestyoona1@gmail.com>
2025-09-18 14:24:25 +0900

    feat: lesson6 mini project

--- lesson6-react-app-miniproject/src/data/data.js
+++ lesson6-react-app-miniproject/src/data/data.js
@@ -35,41 +35,27 @@ export const EXAMPLES = {
     title: "Components",
     description:
       "Component는 React 애플리케이션의 구성 요소입니다. 하나의 독립적인 모듈(HTML + 선택적 CSS + JS)로서 특정한 출력을 렌더링합니다.",
-    code: `
-function Welcome() {
-  return <h1>Hello, World!</h1>;
-}`,
+    code: ` function Welcome() {
+  return <h1>Hello, World!</h1>;}`,
   },
   jsx: {
     title: "JSX",
     description:
       "JSX는 JavaScript의 확장 문법입니다. 템플릿 언어와 유사하지만 JavaScript의 모든 기능을 사용할 수 있습니다(예: 동적 콘텐츠 출력).",
-    code: `
-<div>
-  <h1>환영합니다 {userName}님</h1>
-  <p>React를 배워보세요!</p>
-</div>`,
+    code: `<div> <h1>환영합니다 {userName}님</h1> <p>React를 배워보세요!</p> </div>`,
   },
   props: {
     title: "Props",
     description:
       "Component는 props라고 불리는 임의의 입력값을 받아들입니다. 함수의 매개변수와 같은 역할을 합니다.",
-    code: `
-function Welcome(props) {
-  return <h1>안녕하세요, {props.name}님!</h1>;
-}`,
+    code: ` function Welcome(props) { return <h1>안녕하세요, {props.name}님!</h1>; }`,
   },
   state: {
     title: "State",
     description:
       "State는 React Component가 사용자 액션, 네트워크 응답, 기타 요인에 따라 시간이 지남에 따라 출력을 변경할 수 있게 해줍니다.",
-    code: `
-function Counter() {
-  const [isVisible, setIsVisible] = useState(false);
-
-  function handleClick() {
-    setIsVisible(true);
-  }
+    code: ` function Counter() { const [isVisible, setIsVisible] = useState(false); function handleClick() {
+    setIsVisible(true);}
 
   return (
     <div>
