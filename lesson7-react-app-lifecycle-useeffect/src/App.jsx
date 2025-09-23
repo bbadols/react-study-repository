f1af0157fc52eeced1449164b10814be5b4c6f36
kgy-inst <bestyoona1@gmail.com>
2025-09-23 10:27:26 +0900

    feat: react useeffect,api,lifecycle

--- lesson7-react-app-lifecycle-useeffect/src/App.jsx
+++ lesson7-react-app-lifecycle-useeffect/src/App.jsx
@@ -1,21 +1,25 @@
 import React, { useState } from "react";
 import "./App.css";
-import LifecycleDemo from "./components/LifecycleDemo";
+import LifecyleDemo from "./components/LifecyleDemo";
 
 const App = () => {
-  // 버튼 보이는 상태를 저장하기 위한 useState hook을 정의
+  // 버튼 보이는 상태를 저장하기 위한 useState hook 정의
+  // 배열 첫번째 상태변수, 배열 두번째 상태함수
   const [showComponent, setShowComponent] = useState(true);
 
   return (
     <div className="app-container">
       <h2>useEffect 라이프사이클 학습</h2>
-      {/* <LifcyleDemo /> */}
-      {/* 언마운트 테스트를 위한 버튼 */}
-      <button className="toggle-btn" onClick={()=>setShowComponent(!showComponent)}>
-        LifecycleDemo 컴포넌트 {showComponent? '숨기기':'보이기'}
+      {/* <LifecyleDemo /> */}
+      {/* 언마운트 테스트를 위한 버튼  onClick=화살표함수 : 이벤트 핸들러 binding 등록  */}
+      <button
+        className="toggle-btn"
+        onClick={() => setShowComponent(!showComponent)}
+      >
+        LifecycleDemo 컴포넌트 {showComponent ? "숨기기" : "보이기"}
       </button>
-      {/* showComponent 상태가 true 일 때만 LifecycleDemo를 렌더링 */}
-      {showComponent && <LifecycleDemo />}
+      {/* showComponent 상태가 true 일때만 LifecyleDemo를 렌더링 */}
+      {showComponent && <LifecyleDemo />}
     </div>
   );
 };
